89186bf5fab3c3210dba47abf2b9e61312b1c8ca
Mak <mak.terbovc@gmail.com>
2025-08-04 20:39:38 +0200

    changed buttons into primary and secondary

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import "./App.css";
 import Button from "./components/Button";
-import trashIcon from './assets/icons/trash.svg';
+import trashIcon from "./assets/icons/trash.svg";
 
 function App() {
   return (
     <>
-      <div style={{ display: 'flex', gap: '10px' }}>
-        <Button variant="white">Button with text</Button>
-        <Button variant="default" icon={trashIcon}>Button with icon + text</Button>
-        <Button variant="default" icon={trashIcon} />  {/* icon only button */}
+      <div style={{ display: "flex", gap: "10px" }}>
+        <Button variant="primary">Button with text</Button>
+        <Button variant="secondary" icon={trashIcon}>
+          Button with icon + text
+        </Button>
+        <Button variant="secondary" icon={trashIcon} />
       </div>
     </>
   );
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,59 +1,64 @@
-import c from './Button.module.scss';
-import clsx from 'clsx';
+import c from "./Button.module.scss";
+import clsx from "clsx";
 
-    type ButtonProps = {
-        children?: React.ReactNode;
-        variant?: 'default' | 'white';
-        icon?: string;
-    } & React.ButtonHTMLAttributes<HTMLButtonElement>;
+type ButtonProps = {
+  children?: React.ReactNode;
+  variant?: "primary" | "secondary" | "outlined";
+  icon?: string;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-    const Button: React.FC<ButtonProps> = ({
-        children,
-        variant='default',
-        className,
-        icon,
-        ...handlers
-    }) => {
+const Button: React.FC<ButtonProps> = ({
+  children,
+  variant = "primary",
+  className,
+  icon,
+  ...handlers
+}) => {
+  if (icon && !children) {
+    return (
+      <button
+        className={clsx(c.button, c.iconOnly, {
+          [c.primary]: variant === "primary",
+          [c.secondary]: variant === "secondary",
+        })}
+        {...handlers}
+      >
+        <div className={c.content}>
+          <img className={c.icon} src={icon} alt="" />
+        </div>
+      </button>
+    );
+  }
 
-        if (icon && !children){
-            return(
-            <button className={
-                clsx(c.button, c.iconOnly, {
-                    [c.white]: variant === 'white',
-                })}
-                {...handlers}> 
-                <div className={c.content}>
-                    <img className={c.icon} src={icon} alt='' />
-                </div>  
-            </button>
-            );
-        }
+  if (icon) {
+    return (
+      <button
+        className={clsx(c.button, className, {
+          [c.primary]: variant === "primary",
+          [c.secondary]: variant === "secondary",
+        })}
+        {...handlers}
+      >
+        <div className={c.content}>
+          <span>{children}</span>
+          <img className={c.icon} src={icon} alt="" />
+        </div>
+      </button>
+    );
+  }
 
-        if (icon) {
-            return(
-            <button className={
-                clsx(c.button, className, {
-                    [c.white]: variant === 'white',
-                })}
-                {...handlers}> 
-                <div className={c.content}>
-                    <span>{children}</span>
-                    <img className={c.icon} src={icon} alt='' />
-                </div>  
-            </button>
-            );
-        };
-
-        return (
-            <button className={
-                clsx(c.button, className, {
-                    [c.white]: variant === 'white',
-                })}
-                {...handlers}>      
-                <div className={c.content}>
-                    <span>{children}</span>
-                </div>
-            </button>
-        );
-    };
+  return (
+    <button
+      className={clsx(c.button, className, {
+        [c.primary]: variant === "primary",
+        [c.secondary]: variant === "secondary",
+      })}
+      {...handlers}
+    >
+      <div className={c.content}>
+        <span>{children}</span>
+      </div>
+    </button>
+  );
+};
 export default Button;
