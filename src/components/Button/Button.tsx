834ba84c552ca75d985a8c308a993f4c43909e36
Mak <mak.terbovc@gmail.com>
2025-08-04 12:25:22 +0200

    added assets and icons folder, added trash.svg as icon

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,6 @@
 import c from './Button.module.scss';
 import clsx from 'clsx';
+import trashIcon from '../../assets/icons/trash.svg'
 
 export default function Button() {
     return <button className={clsx(c.button, c.white)}>Click me</button>;
