d50a362d251266df535c32de758385c306b6dc4b
Mak <mak.terbovc@gmail.com>
2025-08-04 12:45:52 +0200

    added min and max width, button size changable?? valjda

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,5 +3,5 @@ import clsx from 'clsx';
 import trashIcon from '../../assets/icons/trash.svg'
 
 export default function Button() {
-    return <button className={clsx(c.button, c.white)}>Click me</button>;
+    return <button className={clsx(c.button)}>Click me</button>;
 }
