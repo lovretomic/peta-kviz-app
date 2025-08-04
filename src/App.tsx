f8bde71d8fb81ec5952a66574290149d4d7dd521
Mak <mak.terbovc@gmail.com>
2025-08-04 13:47:25 +0200

    added icon support, icon only button is square, changed the buton.tsx into arrow function

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import "./App.css";
 import Button from "./components/Button";
+import trashIcon from './assets/icons/trash.svg';
 
 function App() {
   return (
     <>
-      <Button />
+      <div style={{ display: 'flex', gap: '10px' }}>
+        <Button variant="white">Button with text</Button>
+        <Button variant="default" icon={trashIcon}>Button with icon + text</Button>
+        <Button variant="default" icon={trashIcon} />  {/* icon only button */}
+      </div>
     </>
   );
 }
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,7 +1,59 @@
 import c from './Button.module.scss';
 import clsx from 'clsx';
-import trashIcon from '../../assets/icons/trash.svg'
 
-export default function Button() {
-    return <button className={clsx(c.button)}>Click me</button>;
-}
+    type ButtonProps = {
+        children?: React.ReactNode;
+        variant?: 'default' | 'white';
+        icon?: string;
+    } & React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+    const Button: React.FC<ButtonProps> = ({
+        children,
+        variant='default',
+        className,
+        icon,
+        ...handlers
+    }) => {
+
+        if (icon && !children){
+            return(
+            <button className={
+                clsx(c.button, c.iconOnly, {
+                    [c.white]: variant === 'white',
+                })}
+                {...handlers}> 
+                <div className={c.content}>
+                    <img className={c.icon} src={icon} alt='' />
+                </div>  
+            </button>
+            );
+        }
+
+        if (icon) {
+            return(
+            <button className={
+                clsx(c.button, className, {
+                    [c.white]: variant === 'white',
+                })}
+                {...handlers}> 
+                <div className={c.content}>
+                    <img className={c.icon} src={icon} alt='' />
+                    <span>{children}</span>
+                </div>  
+            </button>
+            );
+        };
+
+        return (
+            <button className={
+                clsx(c.button, className, {
+                    [c.white]: variant === 'white',
+                })}
+                {...handlers}>      
+                <div className={c.content}>
+                    <span>{children}</span>
+                </div>
+            </button>
+        );
+    };
+export default Button;
