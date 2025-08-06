81851a1b33e1c4a2bdd3bc858414c9b50f99a24a
Lovre Tomić <tomiclovre05@gmail.com>
2025-08-06 18:47:22 +0200

    added test page

--- src/Router.tsx
+++ src/Router.tsx
@@ -7,6 +7,7 @@ import {
 import QuizzesPage from "./pages/QuizzesPage";
 import LeaderboardPage from "./pages/LeaderboardPage";
 import ApplicationPage from "./pages/ApplicationPage";
+import TestPage from "./pages/TestPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -15,6 +16,7 @@ const router = createBrowserRouter(
       <Route path="/quizzes" element={<QuizzesPage />} />
       <Route path="/leaderboard" element={<LeaderboardPage />} />
       <Route path="/apply" element={<ApplicationPage />} />
+      <Route path="/test" element={<TestPage />} />
       <Route path="*" element={<h1>404 Not Found</h1>} />
     </>
   )
