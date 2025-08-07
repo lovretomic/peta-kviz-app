9914baff86ef54b181b60d3c7ad1c506f993153b
Lovre Tomić <tomiclovre05@gmail.com>
2025-08-07 15:57:36 +0200

    refactored to mobile navigation

---
+++ src/components/NavigationButton/NavigationButton.tsx
@@ -0,0 +1,26 @@
+import { useNavigate } from "react-router-dom";
+import type { NavigationItem } from "../../router/navigationItems";
+import c from "./NavigationButton.module.scss";
+import clsx from "clsx";
+
+type NavigationItemProps = {
+  item: NavigationItem;
+};
+
+const NavigationButton = ({ item }: NavigationItemProps) => {
+  const navigate = useNavigate();
+  const route = window.location.pathname;
+
+  return (
+    <button
+      className={clsx({ [c.active]: route === item.path }, c.button)}
+      onClick={() => navigate(item.path)}
+      key={item.name}
+    >
+      <img src={item.icon} alt={`${item.name} icon`} />
+      {item.name}
+    </button>
+  );
+};
+
+export default NavigationButton;
--- src/pages/TestPage/TestPage.tsx
+++ src/pages/TestPage/TestPage.tsx
@@ -1,4 +1,4 @@
-import Navigation from "../../components/Navigation";
+import Navigation from "../../components/MobileNavigation";
 
 const TestPage = () => {
   return <Navigation />;
