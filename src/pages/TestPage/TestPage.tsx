23e50e186d2e2be35abb815940bda44a667a4047
Lovre Tomić <tomiclovre05@gmail.com>
2025-08-07 20:05:51 +0200

    Update TestPage.tsx

--- src/pages/TestPage/TestPage.tsx
+++ src/pages/TestPage/TestPage.tsx
@@ -1,5 +1,22 @@
+import Button from "../../components/Button";
+import Input from "../../components/Input";
+import trashIcon from "../../assets/icons/trash.svg";
+
 const TestPage = () => {
-  return <h1>Test Page</h1>;
+  return (
+    <>
+      <div style={{ display: "flex", gap: "10px" }}>
+        <Button variant="primary">Button with text</Button>
+        <Button variant="outlined" icon={trashIcon}>
+          Button with icon + text
+        </Button>
+        <Button variant="secondary" icon={trashIcon} />
+      </div>
+      <div style={{ display: "flex", gap: "10px" }}>
+        <Input placeholder="upiši" />
+      </div>
+    </>
+  );
 };
 
 export default TestPage;
