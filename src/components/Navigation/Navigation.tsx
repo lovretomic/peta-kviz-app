1fe1f3b80af8f5100cfce8eda575e579403419e8
Lovre Tomić <tomiclovre05@gmail.com>
2025-08-07 15:43:11 +0200

    added selected state by route

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -2,15 +2,17 @@ import { useNavigate } from "react-router-dom";
 import { navigationItems } from "../../router/navigationItems";
 
 import c from "./Navigation.module.scss";
+import clsx from "clsx";
 
 const Navigation = () => {
   const navigate = useNavigate();
+  const route = window.location.pathname;
 
   return (
     <nav className={c.navigation}>
       {navigationItems.map((item) => (
         <button
-          className={c.item}
+          className={clsx({ [c.active]: route === item.path }, c.item)}
           onClick={() => navigate(item.path)}
           key={item.name}
         >
