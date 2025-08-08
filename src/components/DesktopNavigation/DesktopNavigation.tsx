ab56d3f0a0666d0b844cb125b7cb123f6f8d934f
Lovre Tomić <tomiclovre05@gmail.com>
2025-08-08 11:45:29 +0200

    added logo to desktop navigation

--- src/components/DesktopNavigation/DesktopNavigation.tsx
+++ src/components/DesktopNavigation/DesktopNavigation.tsx
@@ -1,10 +1,13 @@
+import {} from "../../hooks/useIsDesktop";
 import { navigationItems } from "../../router/navigationItems";
 import NavigationButton from "../NavigationButton/NavigationButton";
 import c from "./DesktopNavigation.module.scss";
+import Logo from "../../assets/logo.svg";
 
 const DesktopNavigation = () => {
   return (
     <nav className={c.desktopNavigation}>
+      <img className={c.logo} src={Logo} />
       {navigationItems.map((item) => (
         <NavigationButton key={item.name} item={item} isDesktop />
       ))}
