e4433357b2d738111ad03908d4ace9fee7880444
Lovre Tomić <tomiclovre05@gmail.com>
2025-08-08 11:28:18 +0200

    added getting title

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,21 @@
+import { useLocation } from "react-router-dom";
+import Logo from "../../assets/logo.svg";
+import c from "./Header.module.scss";
+import { navigationItems } from "../../router/navigationItems";
+
 const Header = () => {
-  return <header>header</header>;
+  const location = useLocation();
+  const title = navigationItems.find(
+    (item) => item.path === location.pathname
+  )?.name;
+
+  return (
+    <header className={c.header}>
+      <img className={c.logo} src={Logo} alt="Logo" />
+      <div className={c.separator} />
+      <h1 className={c.title}>{title}</h1>
+    </header>
+  );
 };
 
 export default Header;
--- src/pages/QuizzesPage/QuizzesPage.tsx
+++ src/pages/QuizzesPage/QuizzesPage.tsx
@@ -1,5 +1,5 @@
 const QuizzesPage = () => {
-  return <h1>Quizzes Page</h1>;
+  return <></>;
 };
 
 export default QuizzesPage;
