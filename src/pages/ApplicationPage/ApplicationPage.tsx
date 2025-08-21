1763460872559bc1706ad9b137f4230561b1c126
Lovre Tomić <tomiclovre05@gmail.com>
2025-08-21 16:37:58 +0200

    added content

--- src/pages/ApplicationPage/ApplicationPage.tsx
+++ src/pages/ApplicationPage/ApplicationPage.tsx
@@ -1,5 +1,17 @@
+import Hourglass from "../../assets/icons/hourglass.svg";
+import c from "./ApplicationPage.module.css";
+
 const ApplicationPage = () => {
-  return <></>;
+  return (
+    <div className={c.container}>
+      <img src={Hourglass} alt="Pješčani sat" />
+      <h2>Ima još vremena.</h2>
+      <p>
+        Kvizevi se održavaju svakog posljednjeg petka u mjesecu. Prijave se
+        otvaraju nekoliko dana prije kviza.
+      </p>
+    </div>
+  );
 };
 
 export default ApplicationPage;
