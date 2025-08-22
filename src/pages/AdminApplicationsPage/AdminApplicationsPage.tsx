a1c5e72b327e616b8b488033a25be86b7e727d82
Lovre Tomić <tomiclovre05@gmail.com>
2025-08-22 18:32:54 +0200

    added vertical separator resizing

--- src/pages/AdminApplicationsPage/AdminApplicationsPage.tsx
+++ src/pages/AdminApplicationsPage/AdminApplicationsPage.tsx
@@ -1,5 +1,51 @@
+import { useRef, useState } from "react";
+import c from "./AdminApplicationsPage.module.scss";
+
 const AdminApplicationsPage = () => {
-  return <></>;
+  const leftRef = useRef<HTMLDivElement>(null);
+
+  const [isDragging, setIsDragging] = useState<boolean>(false);
+  const [startWidth, setStartWidth] = useState<number>(0);
+  const [width, setWidth] = useState<number>(300);
+
+  const handleMouseDown = () => {
+    setIsDragging(true);
+    setStartWidth(leftRef.current?.getBoundingClientRect().width || 0);
+  };
+
+  const handleMouseMove = (e: React.MouseEvent) => {
+    if (isDragging) {
+      const newWidth = e.clientX - 8;
+
+      if (newWidth > 100 && newWidth < window.innerWidth - 100) {
+        setWidth(newWidth);
+      }
+    }
+  };
+
+  const handleMouseUp = () => {
+    setIsDragging(false);
+  };
+
+  return (
+    <div
+      className={c.adminApplicationsPage}
+      onMouseMove={(e) => handleMouseMove(e)}
+      onMouseUp={handleMouseUp}
+      onMouseLeave={handleMouseUp}
+    >
+      <main className={c.main} ref={leftRef} style={{ width, color: "black" }}>
+        {`startWidth: ${startWidth}
+            width: ${width}
+        `}
+      </main>
+      <div className={c.hSeparator} onMouseDown={handleMouseDown} />
+      <aside
+        className={c.aside}
+        style={{ width: `calc(100% - ${width}px)` }}
+      ></aside>
+    </div>
+  );
 };
 
 export default AdminApplicationsPage;
--- src/router/AdminLayout/AdminLayout.tsx
+++ src/router/AdminLayout/AdminLayout.tsx
@@ -1,15 +1,25 @@
 import { Outlet } from "react-router-dom";
 import c from "./AdminLayout.module.scss";
 import AdminNavButton from "../../components/AdminNavButton";
+import { useState } from "react";
 
 const AdminLayout = () => {
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+
+  function handleMouseMove(event: React.MouseEvent) {
+    setX(event.clientX - 8);
+    setY(event.clientY);
+  }
+
   return (
-    <div className={c.adminLayout}>
+    <div className={c.adminLayout} onMouseMove={handleMouseMove}>
       <nav className={c.navigation}>
         <div className={c.left}>
           <AdminNavButton>Admin</AdminNavButton>
           <AdminNavButton>Kviz</AdminNavButton>
           <AdminNavButton>Prijave</AdminNavButton>
+          <p className={c.debugInfo}>{`X: ${x}, Y: ${y}`}</p>
         </div>
         <div className={c.right}>
           <p>Administrator</p>
