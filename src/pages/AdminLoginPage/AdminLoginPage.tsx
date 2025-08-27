c7996efa6761b5c624c12493704a2e532152a773
Lovre Tomić <tomiclovre05@gmail.com>
2025-08-27 15:51:35 +0200

    improved responsiveness

--- src/pages/AdminLoginPage/AdminLoginPage.tsx
+++ src/pages/AdminLoginPage/AdminLoginPage.tsx
@@ -5,16 +5,18 @@ import c from "./AdminLoginPage.module.scss";
 
 const AdminLoginPage = () => {
   return (
-    <div className={c.contentWrapper}>
-      <div className={c.logoHeader}>
-        <ClickableLogo />
-        <hr />
-        <h2>Prijava voditelja</h2>
-      </div>
-      <div className={c.formWrapper}>
-        <Input placeholder="Upiši oznaku voditelja" />
-        <Input placeholder="Upiši lozinku" type="password" />
-        <Button>Prijavi se</Button>
+    <div className={c.pageWrapper}>
+      <div className={c.contentWrapper}>
+        <div className={c.logoHeader}>
+          <ClickableLogo />
+          <hr />
+          <h2>Prijava voditelja</h2>
+        </div>
+        <div className={c.formWrapper}>
+          <Input placeholder="Upiši oznaku voditelja" />
+          <Input placeholder="Upiši lozinku" type="password" />
+          <Button>Prijavi se</Button>
+        </div>
       </div>
     </div>
   );
