4fc9fe0bb4c19814f07c0787ff19975d96656502
Lovre Tomić <tomiclovre05@gmail.com>
2025-08-27 15:47:39 +0200

    added content

--- src/pages/AdminLoginPage/AdminLoginPage.tsx
+++ src/pages/AdminLoginPage/AdminLoginPage.tsx
@@ -1,7 +1,21 @@
+import Button from "../../components/Button";
+import ClickableLogo from "../../components/ClickableLogo";
+import Input from "../../components/Input";
+import c from "./AdminLoginPage.module.scss";
+
 const AdminLoginPage = () => {
   return (
-    <div>
-      <h1>Admin Login</h1>
+    <div className={c.contentWrapper}>
+      <div className={c.logoHeader}>
+        <ClickableLogo />
+        <hr />
+        <h2>Prijava voditelja</h2>
+      </div>
+      <div className={c.formWrapper}>
+        <Input placeholder="Upiši oznaku voditelja" />
+        <Input placeholder="Upiši lozinku" type="password" />
+        <Button>Prijavi se</Button>
+      </div>
     </div>
   );
 };
