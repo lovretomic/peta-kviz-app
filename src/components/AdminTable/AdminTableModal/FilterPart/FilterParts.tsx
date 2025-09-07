cd51554e00fe7cbb82c962fbc9611be68bda0564
Lovre Tomić <tomiclovre05@gmail.com>
2025-09-07 18:14:17 +0200

    updated styles

--- src/components/AdminTable/AdminTableModal/FilterPart/FilterParts.tsx
+++ src/components/AdminTable/AdminTableModal/FilterPart/FilterParts.tsx
@@ -5,6 +5,8 @@ import {
   type StringFilterDesc,
 } from "../../types";
 
+import c from "./FilterParts.module.scss";
+
 type StringFilterPartProps = {
   type: "string";
   label?: string;
@@ -30,27 +32,32 @@ const StringFilterPart = ({
   edit,
 }: Omit<StringFilterPartProps, "type">) => {
   return (
-    <div>
-      <p>{descriptor.type}</p>
-      <h4>{label}</h4>
-      <select
-        name=""
-        id=""
-        value={descriptor.op}
-        onChange={(e) => edit?.({ ...descriptor, op: e.target.value as any })}
-      >
-        {StringFilterOps.map((op) => (
-          <option key={op.value} value={op.value}>
-            {op.label}
-          </option>
-        ))}
-      </select>
-      <input
-        type="text"
-        value={descriptor.value}
-        onChange={(e) => edit?.({ ...descriptor, value: e.target.value })}
-      />
-      <button onClick={remove}>Remove</button>
+    <div className={c.filterPart}>
+      <div className={c.header}>
+        <p className={c.type}>{descriptor.type}</p>
+        <h4 className={c.label}>{label}</h4>
+      </div>
+      <div className={c.inputs}>
+        <select
+          name=""
+          id=""
+          value={descriptor.op}
+          onChange={(e) => edit?.({ ...descriptor, op: e.target.value as any })}
+        >
+          {StringFilterOps.map((op) => (
+            <option key={op.value} value={op.value}>
+              {op.label}
+            </option>
+          ))}
+        </select>
+        <input
+          type="text"
+          value={descriptor.value}
+          onChange={(e) => edit?.({ ...descriptor, value: e.target.value })}
+        />
+      </div>
+
+      <button onClick={remove}>Ukloni</button>
     </div>
   );
 };
@@ -62,29 +69,34 @@ const NumberFilterPart = ({
   edit,
 }: Omit<NumberFilterPartProps, "type">) => {
   return (
-    <div>
-      <h4>{label}</h4>
-      <p>Type: {descriptor.type}</p>
-      <select
-        name=""
-        id=""
-        value={descriptor.op}
-        onChange={(e) => edit?.({ ...descriptor, op: e.target.value as any })}
-      >
-        {NumberFilterOps.map((op) => (
-          <option key={op.value} value={op.value}>
-            {op.label}
-          </option>
-        ))}
-      </select>
-      <input
-        type="number"
-        value={descriptor.a}
-        onChange={(e) =>
-          edit?.({ ...descriptor, a: e.target.value as unknown as number })
-        }
-      />
-      <button onClick={remove}>Remove</button>
+    <div className={c.filterPart}>
+      <div className={c.header}>
+        <p className={c.type}>{descriptor.type}</p>
+        <h4 className={c.label}>{label}</h4>
+      </div>
+      <div className={c.inputs}>
+        <select
+          name=""
+          id=""
+          value={descriptor.op}
+          onChange={(e) => edit?.({ ...descriptor, op: e.target.value as any })}
+        >
+          {NumberFilterOps.map((op) => (
+            <option key={op.value} value={op.value}>
+              {op.label}
+            </option>
+          ))}
+        </select>
+        <input
+          type="number"
+          value={descriptor.a}
+          onChange={(e) =>
+            edit?.({ ...descriptor, a: e.target.value as unknown as number })
+          }
+        />
+      </div>
+
+      <button onClick={remove}>Ukloni</button>
     </div>
   );
 };
