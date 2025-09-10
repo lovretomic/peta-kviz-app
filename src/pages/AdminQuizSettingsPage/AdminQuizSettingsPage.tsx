ca45c6cf8f3585d3359f22ae5070ab8d024bb423
Lovre Tomić <tomiclovre05@gmail.com>
2025-09-10 16:44:00 +0200

    improved styles

--- src/pages/AdminQuizSettingsPage/AdminQuizSettingsPage.tsx
+++ src/pages/AdminQuizSettingsPage/AdminQuizSettingsPage.tsx
@@ -1,4 +1,6 @@
 import AdminButton from "../../components/AdminButton";
+import AdminInput from "../../components/AdminInput";
+import AdminSelect from "../../components/AdminSelect";
 import c from "./AdminQuizSettingsPage.module.scss";
 
 const AdminQuizSettingsPage = () => {
@@ -6,44 +8,53 @@ const AdminQuizSettingsPage = () => {
     <div className={c.page}>
       <h1>Postavke</h1>
       <p className={c.description}>Ovdje možete prilagoditi postavke kviza.</p>
-      <div className={c.inputContainer}>
-        <label htmlFor="title">Naslov</label>
-        <input type="text" id="title" />
+      <div className={c.grid}>
+        <div>
+          <div className={c.inputContainer}>
+            <label htmlFor="title">Naslov</label>
+            <AdminInput type="text" id="title" />
+          </div>
+          <div className={c.inputContainer}>
+            <label htmlFor="id">Oznaka</label>
+            <AdminInput type="text" id="id" disabled />
+          </div>
+          <div className={c.inputContainer}>
+            <label htmlFor="status">Status</label>
+            <AdminSelect name="status" id="status">
+              <option value="upcoming">Nadolazeći</option>
+              <option value="ongoing">U tijeku</option>
+              <option value="completed">Završen</option>
+            </AdminSelect>
+          </div>
+          <div className={c.inputContainer}>
+            <label htmlFor="league">Liga</label>
+            <AdminSelect name="league" id="league">
+              <option value="league1">Liga 1</option>
+              <option value="league2">Liga 2</option>
+              <option value="league3">Liga 3</option>
+            </AdminSelect>
+          </div>
+        </div>
+        <div>
+          <div className={c.inputContainer}>
+            <label htmlFor="date">Datum i vrijeme</label>
+            <AdminInput type="datetime-local" id="date" />
+          </div>
+          <div className={c.inputContainer}>
+            <label htmlFor="location">Lokacija</label>
+            <AdminInput type="text" id="location" />
+          </div>
+          <div className={c.inputContainer}>
+            <label htmlFor="capacity">Broj mjesta</label>
+            <AdminInput type="number" id="capacity" />
+          </div>
+        </div>
       </div>
-      <div className={c.inputContainer}>
-        <label htmlFor="id">Oznaka</label>
-        <input type="text" id="id" disabled />
+
+      <div className={c.buttons}>
+        <AdminButton variant="secondary">Povratak</AdminButton>
+        <AdminButton>Spremi promjene</AdminButton>
       </div>
-      <div className={c.inputContainer}>
-        <label htmlFor="status">Status</label>
-        <select name="status" id="status">
-          <option value="upcoming">Nadolazeći</option>
-          <option value="ongoing">U tijeku</option>
-          <option value="completed">Završen</option>
-        </select>
-      </div>
-      <div className={c.inputContainer}>
-        <label htmlFor="league">Liga</label>
-        <select name="league" id="league">
-          <option value="league1">Liga 1</option>
-          <option value="league2">Liga 2</option>
-          <option value="league3">Liga 3</option>
-        </select>
-      </div>
-      <div className={c.inputContainer}>
-        <label htmlFor="date">Datum i vrijeme</label>
-        <input type="datetime-local" id="date" />
-      </div>
-      <div className={c.inputContainer}>
-        <label htmlFor="location">Lokacija</label>
-        <input type="text" id="location" />
-      </div>
-      <div className={c.inputContainer}>
-        <label htmlFor="capacity">Broj mjesta</label>
-        <input type="number" id="capacity" />
-      </div>
-      <AdminButton variant="secondary">Povratak</AdminButton>
-      <AdminButton>Spremi promjene</AdminButton>
     </div>
   );
 };
