fb288cf1108c1d75759e77c3ab7c3d2120b7549c
lovretomic <92167015+lovretomic@users.noreply.github.com>
2025-09-12 00:17:02 +0200

    changed scrolling behaviour

--- src/pages/ApplicationForm/ApplicationForm.tsx
+++ src/pages/ApplicationForm/ApplicationForm.tsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import Button from "../../components/Button";
 import Input from "../../components/Input";
 import MemberList from "../../components/MemberList";
 import ProgressBar from "../../components/ProgressBar";
 import c from "./ApplicationForm.module.scss";
 
+type Application = {
+  teamName: string;
+  captainName: string;
+  captainEmail: string;
+  members: string[];
+  appliedAt: Date;
+};
+
 const ApplicationForm = () => {
+  const [application, setApplication] = useState<Application | null>(null);
+
   return (
     <>
       <div className={c.ellipse} />
@@ -15,19 +26,58 @@ const ApplicationForm = () => {
           date={new Date()}
         />
         <h2>Prijava ekipe</h2>
-        <div className={c.scroller}>
+        <Input
+          placeholder="Upiši ime ekipe"
+          style={{ marginBottom: "24px" }}
+          value={application?.teamName || ""}
+          onChange={(e) =>
+            setApplication({
+              ...application,
+              teamName: e.target.value as string,
+            } as Application)
+          }
+        />
+        <p>Informacije o kapetanu</p>
+        <div className={c.inputGroup}>
           <Input
-            placeholder="Upiši ime ekipe"
-            style={{ marginBottom: "24px" }}
+            placeholder="Upiši ime i prezime kapetana"
+            value={application?.captainName || ""}
+            onChange={(e) =>
+              setApplication({
+                ...application,
+                captainName: e.target.value as string,
+              } as Application)
+            }
           />
-          <p>Informacije o kapetanu</p>
-          <div className={c.inputGroup}>
-            <Input placeholder="Upiši ime i prezime kapetana" />
-            <Input placeholder="Upiši e-adresu kapetana" />
-          </div>
-
-          <MemberList captainName="Ime kapetana" members={[]} />
+          <Input
+            placeholder="Upiši e-adresu kapetana"
+            value={application?.captainEmail || ""}
+            onChange={(e) =>
+              setApplication({
+                ...application,
+                captainEmail: e.target.value as string,
+              } as Application)
+            }
+          />
         </div>
+
+        <MemberList
+          captainName={application?.captainName || ""}
+          members={application?.members || []}
+          addMember={(name) =>
+            setApplication({
+              ...application,
+              members: [...(application?.members || []), name],
+            } as Application)
+          }
+          removeMember={(name) =>
+            setApplication({
+              ...application,
+              members: (application?.members || []).filter((m) => m !== name),
+            } as Application)
+          }
+          maxMembers={5}
+        />
         <Button variant="primary" className={c.submitButton}>
           Pošalji prijavu
         </Button>
