364fd5724a809a909b30636651249f781304584a
lovretomic <92167015+lovretomic@users.noreply.github.com>
2025-09-11 19:01:53 +0200

    improved responsiveness and layout

--- src/pages/ApplicationForm/ApplicationForm.tsx
+++ src/pages/ApplicationForm/ApplicationForm.tsx
@@ -1,7 +1,34 @@
+import Button from "../../components/Button";
+import Input from "../../components/Input";
+import MemberList from "../../components/MemberList";
+import ProgressBar from "../../components/ProgressBar";
 import c from "./ApplicationForm.module.scss";
 
 const ApplicationForm = () => {
-  return <div className={c.container}></div>;
+  return (
+    <div className={c.container}>
+      <div className={c.ellipse} />
+      <ProgressBar
+        percentage={50}
+        label="Kviz općeg znanja"
+        date={new Date()}
+      />
+      <h2>Prijava ekipe</h2>
+      <div className={c.scroller}>
+        <Input placeholder="Upiši ime ekipe" style={{ marginBottom: "24px" }} />
+        <p>Informacije o kapetanu</p>
+        <div className={c.inputGroup}>
+          <Input placeholder="Upiši ime i prezime kapetana" />
+          <Input placeholder="Upiši e-adresu kapetana" />
+        </div>
+
+        <MemberList captainName="Ime kapetana" members={[]} />
+      </div>
+      <Button variant="primary" className={c.submitButton}>
+        Pošalji prijavu
+      </Button>
+    </div>
+  );
 };
 
 export default ApplicationForm;
