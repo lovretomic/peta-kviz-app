126b7e27c9358e24d8ce11679962478453598118
lovretomic <92167015+lovretomic@users.noreply.github.com>
2025-09-13 23:52:24 +0200

    added quizzes page base

--- src/pages/QuizzesPage/QuizzesPage.tsx
+++ src/pages/QuizzesPage/QuizzesPage.tsx
@@ -1,5 +1,29 @@
+import PageWrapper from "../../components/PageWrapper";
+import ProgressBar from "../../components/ProgressBar";
+import c from "./QuizzesPage.module.scss";
+
 const QuizzesPage = () => {
-  return <></>;
+  return (
+    <>
+      <div className={c.actionsHeader}>
+        <PageWrapper className={c.wrapper}>
+          <select name="" id="">
+            <option value="">Liga 2024.</option>
+            <option value="">Liga 2025.</option>
+          </select>
+          <button>Sortiraj</button>
+        </PageWrapper>
+      </div>
+      <PageWrapper className={c.pageWrapper}>
+        <ProgressBar
+          label="Loading..."
+          percentage={45}
+          date={new Date()}
+          variant="withButtons"
+        />
+      </PageWrapper>
+    </>
+  );
 };
 
 export default QuizzesPage;
