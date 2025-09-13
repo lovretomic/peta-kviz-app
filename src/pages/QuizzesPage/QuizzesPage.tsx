30824275dc28940c9336130695fa9f218964d793
lovretomic <92167015+lovretomic@users.noreply.github.com>
2025-09-14 00:52:35 +0200

    fixed scroll direction and layout

--- src/pages/QuizzesPage/QuizzesPage.tsx
+++ src/pages/QuizzesPage/QuizzesPage.tsx
@@ -1,9 +1,11 @@
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
 import PageWrapper from "../../components/PageWrapper";
 import ProgressBar from "../../components/ProgressBar";
 import type { QuizResults } from "../../components/QuizCard/QuizCard";
 import QuizCard from "../../components/QuizCard/QuizCard";
 import c from "./QuizzesPage.module.scss";
+import { useIsDesktop } from "../../hooks/useIsDesktop";
+import clsx from "clsx";
 
 const quizResults: QuizResults[] = [
   {
@@ -21,17 +23,16 @@ const quizResults: QuizResults[] = [
     date: new Date(),
     leaderboard: ["Ivana Ivanković", "Katarina Kovač", "Nikola Novak"],
   },
+  {
+    name: "Kviz 4",
+    date: new Date(),
+    leaderboard: ["Ana Kovač", "Marko Marić", "Ivan Horvat"],
+  },
 ];
 
 const QuizzesPage = () => {
   const wrapperRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const element = wrapperRef.current;
-    if (element) {
-      element.scrollTop = element.scrollHeight;
-    }
-  }, []);
+  const isDesktop = useIsDesktop();
 
   return (
     <div className={c.page}>
@@ -44,11 +45,10 @@ const QuizzesPage = () => {
           <button>Sortiraj</button>
         </PageWrapper>
       </div>
-      <PageWrapper className={c.pageWrapper} ref={wrapperRef}>
-        {quizResults.map((quiz) => (
-          <QuizCard key={quiz.name} quizResults={quiz} />
-        ))}
-
+      <PageWrapper
+        className={clsx(c.pageWrapper, { [c.isDesktop]: isDesktop })}
+        ref={wrapperRef}
+      >
         <ProgressBar
           label="Loading..."
           percentage={45}
@@ -56,6 +56,10 @@ const QuizzesPage = () => {
           buttonLabel="Pregledaj prijavu"
           buttonVariant="secondary"
         />
+
+        {quizResults.map((quiz) => (
+          <QuizCard key={quiz.name} quizResults={quiz} />
+        ))}
       </PageWrapper>
     </div>
   );
