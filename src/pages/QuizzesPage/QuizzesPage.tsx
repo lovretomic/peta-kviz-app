e89e673a348ca122020f5da2a4c447549a309070
lovretomic <92167015+lovretomic@users.noreply.github.com>
2025-09-14 13:54:52 +0200

    added sorting

--- src/pages/QuizzesPage/QuizzesPage.tsx
+++ src/pages/QuizzesPage/QuizzesPage.tsx
@@ -5,33 +5,37 @@ import type { QuizResults } from "../../components/QuizCard/QuizCard";
 import QuizCard from "../../components/QuizCard/QuizCard";
 import c from "./QuizzesPage.module.scss";
 
+import SwapVertIcon from "../../assets/icons/swap-vert.svg?react";
+
 const quizResults: QuizResults[] = [
   {
     name: "Kviz 1",
-    date: new Date(),
+    date: new Date("2024-01-01"),
     leaderboard: ["Ivan Horvat", "Ana Kovač", "Marko Marić"],
   },
   {
     name: "Kviz 2",
-    date: new Date(),
+    date: new Date("2024-01-02"),
     leaderboard: ["Petar Perić", "Luka Lukić", "Maja Marić"],
   },
   {
     name: "Kviz 3",
-    date: new Date(),
+    date: new Date("2024-01-03"),
     leaderboard: ["Ivana Ivanković", "Katarina Kovač", "Nikola Novak"],
   },
   {
     name: "Kviz 4",
-    date: new Date(),
+    date: new Date("2024-01-04"),
     leaderboard: ["Ana Kovač", "Marko Marić", "Ivan Horvat"],
   },
 ];
 
 const QuizzesPage = () => {
   const wrapperRef = useRef<HTMLDivElement>(null);
-
   const [expandedQuiz, setExpandedQuiz] = useState<string | null>(null);
+  const [sortingDirection, setSortingDirection] = useState<"asc" | "desc">(
+    "asc"
+  );
 
   return (
     <div className={c.page}>
@@ -41,7 +45,14 @@ const QuizzesPage = () => {
             <option value="">Liga 2024.</option>
             <option value="">Liga 2025.</option>
           </select>
-          <button>Sortiraj</button>
+          <button
+            onClick={() => {
+              setSortingDirection(sortingDirection === "asc" ? "desc" : "asc");
+            }}
+          >
+            <SwapVertIcon className={c.icon} />
+            Sortiraj
+          </button>
         </PageWrapper>
       </div>
       <PageWrapper className={c.pageWrapper} ref={wrapperRef}>
@@ -53,16 +64,24 @@ const QuizzesPage = () => {
           buttonVariant="secondary"
         />
 
-        {quizResults.map((quiz) => (
-          <QuizCard
-            key={quiz.name}
-            quizResults={quiz}
-            isOpen={expandedQuiz === quiz.name}
-            onToggle={() => {
-              setExpandedQuiz(expandedQuiz === quiz.name ? null : quiz.name);
-            }}
-          />
-        ))}
+        {quizResults
+          .sort((a, b) => {
+            if (sortingDirection === "asc") {
+              return a.date > b.date ? 1 : -1;
+            } else {
+              return a.date < b.date ? 1 : -1;
+            }
+          })
+          .map((quiz) => (
+            <QuizCard
+              key={quiz.name}
+              quizResults={quiz}
+              isOpen={expandedQuiz === quiz.name}
+              onToggle={() => {
+                setExpandedQuiz(expandedQuiz === quiz.name ? null : quiz.name);
+              }}
+            />
+          ))}
       </PageWrapper>
     </div>
   );
