0713457cdb511157c09f4ce65e1eaf207aa46fdc
Mak <mak.terbovc@gmail.com>
2025-09-16 11:55:49 +0200

    updated scss file, added isOpen prop

--- src/components/CodeModal/CodeModal.tsx
+++ src/components/CodeModal/CodeModal.tsx
@@ -5,6 +5,8 @@ import Input from "../Input";
 type CodeModalProps = {
   date: Date | string;
   code?: string;
+  isOpen: boolean;
+  setIsOpen: (isOpen: boolean) => void;
 } & React.HTMLAttributes<HTMLDivElement>;
 
 function formatDate(date: Date | string): string {
@@ -19,8 +21,23 @@ function formatDate(date: Date | string): string {
   });
 }
 
-const CodeModal: React.FC<CodeModalProps> = ({ date, code }) => {
-  return <div className={c.modalBackground}>{code}</div>;
+const CodeModal: React.FC<CodeModalProps> = ({
+  date,
+  code,
+  isOpen = true,
+  setIsOpen,
+}) => {
+  return (
+    <div className={c.background} style={{ display: isOpen ? "flex" : "none" }}>
+      <div className={c.modal}>
+        <header className={c.header}>
+          <h1>Unos kȏda</h1>
+          <h2>Kviz općeg znanja</h2>
+          <h3>{formatDate(date)}</h3>
+        </header>
+      </div>
+    </div>
+  );
 };
 
 export default CodeModal;
