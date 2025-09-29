3e17a159900d6319ee3037daeec23de48d77b5c1
lovretomic <92167015+lovretomic@users.noreply.github.com>
2025-09-29 17:53:54 +0200

    one more try

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
-export default defineConfig((mode) => {
-  const env = loadEnv(mode.mode, process.cwd(), "VITE_");
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "VITE_");
+
   return {
     define: {
       "import.meta.env": env,
