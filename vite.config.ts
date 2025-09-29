ab50508a2c4e14b16b105e2ecf0eee7fdb8d6759
lovretomic <92167015+lovretomic@users.noreply.github.com>
2025-09-29 17:48:11 +0200

    fixed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,25 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
-export default defineConfig({
-  plugins: [
-    react(),
-    svgr({
-      include: "**/*.svg?react",
-    }),
-  ],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@use "/src/styles/global.scss" as *;`,
+export default defineConfig((mode) => {
+  const env = loadEnv(mode.mode, process.cwd(), "VITE_");
+  return {
+    define: {
+      "import.meta.env": env,
+    },
+    plugins: [
+      react(),
+      svgr({
+        include: "**/*.svg?react",
+      }),
+    ],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "/src/styles/global.scss" as *;`,
+        },
       },
     },
-  },
+  };
 });
