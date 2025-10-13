7bfe963e3dcc048154d389ef54cb087007ddee5f
lovretomic <92167015+lovretomic@users.noreply.github.com>
2025-10-13 16:34:02 +0200

    added info and fixed leading zeros

--- src/components/AdminPointsTable/AdminPointsTable.tsx
+++ src/components/AdminPointsTable/AdminPointsTable.tsx
@@ -15,24 +15,33 @@ const AdminPointsTable = ({ data }: AdminPointsTableProps) => {
   const [rows, setRows] = useState<TeamPointsRow[]>(data);
 
   return (
-    <div className={c.container}>
-      {rows.map((row) => (
-        <>
-          <div>{row.name}</div>
-          <input
-            type="number"
-            value={row.points}
-            onChange={(e) => {
-              const newPoints = Number(e.target.value);
-              setRows((prev) =>
-                prev.map((r) =>
-                  r.id === row.id ? { ...r, points: newPoints } : r
-                )
-              );
-            }}
-          />
-        </>
-      ))}
+    <div className={c.page}>
+      <h1 className={c.title}>Kviz općeg znanja — bodovi</h1>
+      <p className={c.subtitle}>1. 1. 2025. u 19:00 sati.</p>
+      <div className={c.container}>
+        {rows.map((row) => (
+          <>
+            <div>{row.name}</div>
+            <input
+              type="number"
+              value={row.points.toString()}
+              onChange={(e) => {
+                let val = e.target.value;
+
+                val = val.replace(/^0+(?=\d)/, "");
+
+                const newPoints = Number(val);
+
+                setRows((prev) =>
+                  prev.map((r) =>
+                    r.id === row.id ? { ...r, points: newPoints } : r
+                  )
+                );
+              }}
+            />
+          </>
+        ))}
+      </div>
     </div>
   );
 };
